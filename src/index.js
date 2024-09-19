38fb33ee512a15236ec4a7adbd883ae1a6ecbcd1
Alex <alex2323official@gmail.com>
2024-09-19 21:15:12 +0200

    basic structure ready for changing pages with JS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import "./styles.css";
-import { greeting } from "./greeting.js";
+import { Home } from "./pages/Home.js";
+import { Menu } from "./pages/Menu.js";
+import { About } from "./pages/About.js";
 
-console.log(greeting);
+// Main App
+const appDivContainer = document.querySelector("#content");
+const nav = document.querySelector(".nav");
+
+// Start with default Home page
+console.log("Uploading Home template");
+
+nav.addEventListener("click", (item) => {
+  let btnName = item.target.textContent;
+
+  if (btnName == "Home") {
+    console.log("Uploading Home template");
+  } else if (btnName == "About") {
+    console.log("Uploading About template");
+  } else if (btnName == "Menu") {
+    console.log("Uploading Menu template");
+  }
+});
