f117d27c984293d711ef72e5910fd99d216143f3
Alex <alex2323official@gmail.com>
2024-09-19 23:08:06 +0200

    main page finished, 2 to go

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,14 +6,31 @@ export class Home {
     // Clear old web page
     Utility.appDivContainer.innerHTML = "";
 
+    // .home__container
     const homePageContainerDiv = document.createElement("div");
     homePageContainerDiv.classList.add("home__container");
-    homePageContainerDiv.textContent = "HOME PAGE";
+    homePageContainerDiv.style.backgroundImage = `url(${firstImage})`;
+    homePageContainerDiv.style.backgroundSize = "cover";
+    homePageContainerDiv.style.backgroundPosition = "center";
 
-    // Adding images
-    const image1 = document.createElement("img");
-    image1.src = firstImage;
-    homePageContainerDiv.appendChild(image1);
+    // .home__h1
+    const home__h1 = document.createElement("h1");
+    home__h1.classList.add("home__h1");
+    home__h1.textContent = "Thai Food From Boat";
+    homePageContainerDiv.appendChild(home__h1);
+
+    // home__textinfo
+    const home__textinfo = document.createElement("p");
+    home__textinfo.classList.add("home__textinfo");
+    home__textinfo.textContent =
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa iusto aliquid et quae. Placeat autem quos corporis in numquam! Debitis recusandae, enim earum cupiditate eos placeat itaque repellat nostrum sint.hai Food From Boat";
+    homePageContainerDiv.appendChild(home__textinfo);
+
+    // home__cta
+    const home__cta = document.createElement("button");
+    home__cta.classList.add("home__cta");
+    home__cta.textContent = "Place an order";
+    homePageContainerDiv.appendChild(home__cta);
 
     Utility.appDivContainer.appendChild(homePageContainerDiv);
   }
